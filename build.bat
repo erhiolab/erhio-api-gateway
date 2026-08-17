2893773753587f558382ebd6976bbf06f5dd7800
Erhai-lake <erhai.lake.0714@gmail.com>
2026-08-17 19:01:17 +0800

    更新

--- build.bat
+++ build.bat
@@ -2,8 +2,8 @@ set CGO_ENABLED=0
 
 set GOOS=windows
 set GOARCH=amd64
-go build -o elake-api-gateway-windows.exe ./cmd/server
+go build -o erhio-api-gateway-windows.exe ./cmd/server
 
 set GOOS=linux
 set GOARCH=amd64
-go build -o elake-api-gateway-linux ./cmd/server
+go build -o erhio-api-gateway-linux ./cmd/server
